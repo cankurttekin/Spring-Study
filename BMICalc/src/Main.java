6a3e7373679b9aca4bebb01c01d716c8c8b7fd44
cankurttekin <cankurttekin@gmail.com>
2023-12-12 01:23:36 -0500

    BMI Calculator

---
+++ BMICalc/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = reader.nextLine();
+
+        System.out.print("Age: ");
+        int age = Integer.valueOf(reader.nextLine());
+
+        System.out.print("Height: ");
+        double height = Double.valueOf(reader.nextLine());
+
+        System.out.print("Weight: ");
+        double weight = Double.valueOf(reader.nextLine());
+
+        Person person = new Person(name, age, height, weight);
+        System.out.println(person);
+    }
+}
