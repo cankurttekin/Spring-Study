442a12b5d71c1cb3087fd6669a55200b533233ae
cankurttekin <cankurttekin@gmail.com>
2024-05-19 00:52:34 +0300

    configuring the spring boot server

--- Spring/01-spring-boot-overview/06-properties-demo/src/main/java/com/can/springboot/demo/myapp/rest/MyRestController.java
+++ Spring/01-spring-boot-overview/06-properties-demo/src/main/java/com/can/springboot/demo/myapp/rest/MyRestController.java
@@ -1,10 +1,25 @@
 package com.can.springboot.demo.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
+
+    // inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "teaminfo"
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team Name: " + teamName;
+    }
+
     // expose "/" that return "Hello World"
 
     @GetMapping("/")
