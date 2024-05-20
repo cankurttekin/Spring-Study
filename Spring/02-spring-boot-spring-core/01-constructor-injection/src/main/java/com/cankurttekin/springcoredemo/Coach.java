ca8183ef6bad52e21aff6e3675812358652f871c
cankurttekin <cankurttekin@gmail.com>
2024-05-20 11:53:45 +0300

    component scanning

---
+++ Spring/02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/cankurttekin/springcoredemo/Coach.java
@@ -0,0 +1,5 @@
+package com.cankurttekin.springcoredemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ Spring/02-spring-boot-spring-core/02-component-scanning/src/main/java/com/cankurttekin/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,19 @@
+package com.cankurttekin.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+@SpringBootApplication(
+		scanBasePackages = {"com.cankurttekin.springcoredemo",
+							"com.cankurttekin.util"}
+)
+
+ */
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
