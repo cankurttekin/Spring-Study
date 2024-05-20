60ed05532231ec35564b27e5e59f422f70fb332e
cankurttekin <cankurttekin@gmail.com>
2024-05-20 18:02:17 +0300

    qualifiers

--- Spring/02-spring-boot-spring-core/04-qualifiers/src/main/java/com/cankurttekin/springcoredemo/rest/DemoController.java
+++ Spring/02-spring-boot-spring-core/04-qualifiers/src/main/java/com/cankurttekin/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.cankurttekin.springcoredemo.rest;
 
 import com.cankurttekin.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public void setCoach(Coach theCoach) {
+    public DemoController(@Qualifier("baseballCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 
