6b8fc7be1f6d3e103012b35730a64de79883233e
cankurttekin <cankurttekin@gmail.com>
2024-05-21 14:09:54 +0300

    saving a java object with jpa

--- Spring/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/cankurttekin/cruddemo/CruddemoApplication.java
+++ Spring/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/cankurttekin/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.cankurttekin.cruddemo;
 
+import com.cankurttekin.cruddemo.dao.StudentDAO;
+import com.cankurttekin.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,40 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
 		return runner -> {
-			System.out.println("Hello World");
+			//createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+
+		// create multiple students
+		System.out.println("Creating 3 student objects ...");
+		Student tempStudent1 = new Student("John", "Doe", "john@abcde.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@abcde.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@abcde.com");
+
+		// save the student objects
+		System.out.println("Saving the students ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Can", "Kurttekin", "abcd@abcde.com");
+
+		// save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 }
---
+++ Spring/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/cankurttekin/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.cankurttekin.cruddemo.dao;
+
+import com.cankurttekin.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+}
