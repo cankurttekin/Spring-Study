e84d6dd0d32fc2be4c6f162b26aecf6f9882bd2f
cankurttekin <cankurttekin@gmail.com>
2024-05-21 16:29:46 +0300

    querying objects with jpa

--- Spring/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/cankurttekin/cruddemo/dao/StudentDAO.java
+++ Spring/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/cankurttekin/cruddemo/dao/StudentDAO.java
@@ -2,9 +2,15 @@ package com.cankurttekin.cruddemo.dao;
 
 import com.cankurttekin.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
 }
