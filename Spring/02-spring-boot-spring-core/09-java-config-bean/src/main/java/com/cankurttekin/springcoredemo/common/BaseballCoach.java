cca50aa73228fbd94b5652534ec23ad33fc25945
cankurttekin <cankurttekin@gmail.com>
2024-05-21 11:54:51 +0300

    java config bean

---
+++ Spring/02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/cankurttekin/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,19 @@
+package com.cankurttekin.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+    public BaseballCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes in batting practice.";
+    }
+}
