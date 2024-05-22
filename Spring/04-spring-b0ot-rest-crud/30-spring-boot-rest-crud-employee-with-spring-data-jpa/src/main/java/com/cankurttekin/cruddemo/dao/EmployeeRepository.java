45bf2ccc08e9b68493bb7eef12aaa1d9cc42b617
cankurttekin <cankurttekin@gmail.com>
2024-05-22 20:31:20 +0300

    spring data jpa

---
+++ Spring/04-spring-b0ot-rest-crud/30-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/cankurttekin/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.cankurttekin.cruddemo.dao;
+
+import com.cankurttekin.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
