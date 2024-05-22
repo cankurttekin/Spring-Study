0844d8009083c0a4aa01ddab0bfd3f55f4a6ebba
cankurttekin <cankurttekin@gmail.com>
2024-05-22 21:48:15 +0300

    spring data rest configs and sorting

--- Spring/04-spring-b0ot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/cankurttekin/cruddemo/dao/EmployeeRepository.java
+++ Spring/04-spring-b0ot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/cankurttekin/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.cankurttekin.cruddemo.dao;
 
 import com.cankurttekin.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
