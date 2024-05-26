f6d634b1792c58f55fb6669ee781ce9b84cafe3f
cankurttekin <cankurttekin@gmail.com>
2024-05-26 22:46:38 +0300

    spring mvc form data binding drow down lists

--- Spring/07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/cankurttekin/springboot/thymeleafdemo/controller/StudentController.java
+++ Spring/07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/cankurttekin/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,14 +1,20 @@
 package com.cankurttekin.springboot.thymeleafdemo.controller;
 
 import com.cankurttekin.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
@@ -18,6 +24,7 @@ public class StudentController {
 
         // add student object to the model
         theModel.addAttribute("student", theStudent);
+        theModel.addAttribute("countries", countries);
 
         return "student-form";
     }
