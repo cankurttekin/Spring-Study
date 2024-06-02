5affef1868e42eaecb904694313cec74e75576ae
cankurttekin <cankurttekin@gmail.com>
2024-06-03 00:02:36 +0300

    one to many update instructor

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/cankurttekin/cruddemo/dao/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/cankurttekin/cruddemo/dao/AppDAOImpl.java
@@ -94,4 +94,10 @@ public class AppDAOImpl implements AppDAO{
         return instructor;
     }
 
+    @Override
+    @Transactional
+    public void update(Instructor tempInstructor) {
+        entityManager.merge(tempInstructor);
+    }
+
 }
