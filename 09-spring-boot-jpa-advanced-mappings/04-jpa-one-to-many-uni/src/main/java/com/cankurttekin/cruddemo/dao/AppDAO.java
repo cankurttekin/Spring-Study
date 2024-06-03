04c2f2d3b3035a87d2d927c2b4036b468857e246
cankurttekin <cankurttekin@gmail.com>
2024-06-03 16:49:30 +0300

    one to many uni-directional retrieve course and reviews

--- 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/cankurttekin/cruddemo/dao/AppDAO.java
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/cankurttekin/cruddemo/dao/AppDAO.java
@@ -19,4 +19,5 @@ public interface AppDAO {
     Course findCourseById(int theId);
     void deleteCourseById(int theId);
     void save(Course theCourse);
+    Course findCourseAndReviewsByCourseId(int theId);
 }
