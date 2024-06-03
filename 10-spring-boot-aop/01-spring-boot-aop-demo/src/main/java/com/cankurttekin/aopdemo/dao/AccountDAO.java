9d35ab176c2d14bf266a4da7ea72da0f8c22257f
cankurttekin <cankurttekin@gmail.com>
2024-06-03 23:54:53 +0300

    aspect oriented programming - @Before

---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/cankurttekin/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,5 @@
+package com.cankurttekin.aopdemo.dao;
+
+public interface AccountDAO {
+    void addAccount();
+}
---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/cankurttekin/aopdemo/dao/AccountDAOImpl.java
@@ -0,0 +1,12 @@
+package com.cankurttekin.aopdemo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AccountDAOImpl implements AccountDAO {
+
+    @Override
+    public void addAccount() {
+        System.out.println(getClass() + ": DOING DB WORK: ADDING AN ACCOUNT");
+    }
+}
