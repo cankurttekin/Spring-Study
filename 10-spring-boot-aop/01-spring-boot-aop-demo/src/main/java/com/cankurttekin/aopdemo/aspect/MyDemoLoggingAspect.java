2247e917330fbbc3496e1bfc16e2e676689934da
cankurttekin <cankurttekin@gmail.com>
2024-06-04 11:15:21 +0300

    match return type

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/cankurttekin/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/cankurttekin/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,7 +9,8 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
     // this is where we add all of our related advices for logging
 
-    @Before("execution(public void add*())")
+    //@Before("execution(public void add*())")
+    @Before("execution(void add*())")
     public void beforeAddAccountAdvice() {
         System.out.println("\n======>>>> Executing @Before advice on addAccount()");
     }
