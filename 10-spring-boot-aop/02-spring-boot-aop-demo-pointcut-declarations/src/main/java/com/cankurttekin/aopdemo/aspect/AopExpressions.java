63731c48383cc217906a8fb64ecbd33bb10f3a43
cankurttekin <cankurttekin@gmail.com>
2024-06-04 12:27:56 +0300

    ordering aspects

---
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/cankurttekin/aopdemo/aspect/AopExpressions.java
@@ -0,0 +1,22 @@
+package com.cankurttekin.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class AopExpressions {
+    @Pointcut("execution(* com.cankurttekin.aopdemo.dao.*.*(..))")
+    public void forDaoPackage() {}
+
+    // create a pointcut for getter methods
+    @Pointcut("execution(* com.cankurttekin.aopdemo.dao.*.get*(..))")
+    public void getter() {}
+
+    // create a pointcut for setter methods
+    @Pointcut("execution(* com.cankurttekin.aopdemo.dao.*.set*(..))")
+    public void setter() {}
+
+    // create pointcut include package exclude getter/setter
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    public void forDaoPackageNoGetterSetter() {}
+}
--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/cankurttekin/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/cankurttekin/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -2,17 +2,15 @@ package com.cankurttekin.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(2)
-public class MyDemoLoggingAspect {
-
+@Order(3)
+public class MyApiAnalyticsAspect {
     @Before("com.cankurttekin.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n======>>>> Executing @Before advice on addAccount()");
+    public void performApiAnalytics() {
+        System.out.println("\n======>>>> Performing API analytics");
     }
 }
